e25243a250834c23a29d2d0060622efb9b9d8fe6
suguang1234 <ryansu0712@gmail.com>
2024-12-29 15:10:09 +0800

    mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+
+(LOOP)
+  
+  @1
+  D=M
+  @END
+  D;JLE
+
+  
+  @0
+  D=M
+  @2
+  M=M+D
+
+  
+  @1
+  M=M-1
+
+  
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
